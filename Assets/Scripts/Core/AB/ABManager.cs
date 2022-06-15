ce09645632fac972d0cb46f93778c74d91f68854
peterwu3156 <peter1599491679@163.com>
2022-06-15 22:34:46 +0800

    AB包管理器

--- Assets/Scripts/Core/AB/ABManager.cs
+++ Assets/Scripts/Core/AB/ABManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ABManager : SingletonAutoMono<ABManager>
 {
@@ -115,11 +116,99 @@ public class ABManager : SingletonAutoMono<ABManager>
         }
     }
 
-    public T LoadRes<T>(string ABName, string resName)
+    /// <summary>
+    /// 同步加载 根据泛型指定类型
+    /// </summary>
+    /// <typeparam name="T">泛型</typeparam>
+    /// <param name="ABName">包名</param>
+    /// <param name="resName">资源名</param>
+    /// <returns></returns>
+    public T LoadRes<T>(string ABName, string resName) where T: Object
     {
         LoadDependencies(ABName);
         //加载资源
-        return null;
+        T obj = ABDic[ABName].LoadAsset<T>(resName);
+        if (obj == gameObject)
+        {
+            return Instantiate(obj);
+        }
+        else
+        {
+            return obj;
+        }
+    }
+
+    /// <summary>
+    /// 异步加载AB包资源
+    /// </summary>
+    /// <param name="ABName">包名</param>
+    /// <param name="resName">资源名</param>
+    /// <param name="callBack">回调函数</param>
+    public void LoadResAsync(string ABName, string resName, UnityAction<Object> callBack)
+    {
+        StartCoroutine(ReallyLoadResAsync(ABName, resName, callBack));
+    }
+
+
+    public void LoadResAsync(string ABName, string resName, System.Type type, UnityAction<Object> callBack)
+    {
+        StartCoroutine(ReallyLoadResAsync(ABName, resName, type, callBack));
+    }
+
+    public void LoadResAsync<T>(string ABName, string resName, UnityAction<T> callBack) where T : Object
+    {
+        StartCoroutine(ReallyLoadResAsync<T>(ABName, resName, callBack));
+    }
+
+    private IEnumerator ReallyLoadResAsync(string ABName, string resName, UnityAction<Object> callBack)
+    {
+        LoadDependencies(ABName);
+  
+        AssetBundleRequest abRequest = ABDic[ABName].LoadAssetAsync(resName);
+        yield return abRequest;
+
+        if (abRequest.asset == gameObject)
+        {
+            callBack(Instantiate(abRequest.asset));
+        }
+        else
+        {
+            callBack(abRequest.asset);
+        }
+    }
+
+    private IEnumerator ReallyLoadResAsync(string ABName, string resName, System.Type type, UnityAction<Object> callBack)
+    {
+        LoadDependencies(ABName);
+
+        AssetBundleRequest abRequest = ABDic[ABName].LoadAssetAsync(resName, type);
+        yield return abRequest;
+
+        if (abRequest.asset == gameObject)
+        {
+            callBack(Instantiate(abRequest.asset));
+        }
+        else
+        {
+            callBack(abRequest.asset);
+        }
+    }
+
+    private IEnumerator ReallyLoadResAsync<T>(string ABName, string resName, UnityAction<T> callBack) where T: Object
+    {
+        LoadDependencies(ABName);
+
+        AssetBundleRequest abRequest = ABDic[ABName].LoadAssetAsync<T>(resName);
+        yield return abRequest;
+
+        if (abRequest.asset == gameObject)
+        {
+            callBack(Instantiate(abRequest.asset) as T);
+        }
+        else
+        {
+            callBack(abRequest.asset as T);
+        }
     }
 
     /// <summary>
@@ -128,7 +217,11 @@ public class ABManager : SingletonAutoMono<ABManager>
     /// <param name="ABName">要卸载的包名</param>
     public void UnLoad(string ABName)
     {
-
+        if(ABDic.ContainsKey(ABName))
+        {
+            ABDic[ABName].Unload(false);
+            ABDic.Remove(ABName);
+        }
     }
 
     /// <summary>
@@ -136,6 +229,9 @@ public class ABManager : SingletonAutoMono<ABManager>
     /// </summary>
     public void ClearAB()
     {
-
+        AssetBundle.UnloadAllAssetBundles(false);
+        ABDic.Clear();
+        mainAB = null;
+        manifest = null;
     }
 }
